7a22bfcf11d7a460ca1bb5d16f31cdf3a1875b57
Walter Ehrenberger <covwalter@gmail.com>
2024-06-24 14:42:12 +0200

    Changed project name

--- run_cordis_extractor.sh
+++ run_cordis_extractor.sh
@@ -3,5 +3,5 @@
 # CHANGE FOR SBATCH
 
 source venv/bin/activate
-export PYTHONPATH=$PYTHONPATH:/Users/wehrenberger/Code/DIGICHer/DIGICHer_Cordis_Pipeline/src
+export PYTHONPATH=$PYTHONPATH:/Users/wehrenberger/Code/DIGICHer/DIGICHer_Pipeline/src
 python src/extractors/run_cordis_extractor.py
