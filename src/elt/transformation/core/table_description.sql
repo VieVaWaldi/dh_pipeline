d039092bbebec2a023fc50a40ac718717f493be5
Walter Ehrenberger <covwalter@gmail.com>
2025-07-06 11:43:27 +0200

    Database stuff

--- src/elt/transformation/core/table_description.sql
+++ src/elt/transformation/core/table_description.sql
@@ -1,6 +1,11 @@
 -----------------------------------------------
 -- ENTITIES COUNT
 
+select * from core.researchoutput 
+WHERE source_system != 'cordis'
+order by publication_date DESC
+limit 100;
+
 select count(*) from core.researchoutput;
 where full_text is null
 limit 1000;
@@ -32,3 +37,14 @@ select count(*) from core.j_researchoutput_journal;
 -- 112_451
 select count(*) from core.j_researchoutput_link;
 -- 895_712
+
+-----------------------------------------------
+-- MINORITIES
+
+select * from core.researchoutput limit 1;
+
+select * from core.researchoutput
+WHERE lower(title) ilike '%ladin%' OR 
+lower(abstract) ilike '%ladin%' OR 
+-- lower(full_text) ilike '%ladin%' OR 
+lower(comment) ilike '%ladin%';
