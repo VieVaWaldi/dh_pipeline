d49fb154c56e0d819d5fb00d57ae16d2e1294dfa
VieVaWaldi <covwalter@gmail.com>
2026-03-03 12:18:24 +0100

    idk old

--- orchestration/sbatch/sbatch_extractor.sh
+++ orchestration/sbatch/sbatch_extractor.sh
@@ -19,4 +19,4 @@ cd /home/lu72hip/DIGICHer/dh_pipeline || exit
 source venv/bin/activate
 
 python src/elt/extraction/run_extractor.py --source "$SOURCE" --query_id "$QUERY_ID"
-#python src/analysis/jobs/file_structure_analysis.py -q cordis_contenttypeISprojectANDSTAR
+python src/analytics/jobs/raw_file_analysis.py --source_query_id cordis-query_id-0
